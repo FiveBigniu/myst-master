12065ea069861bc27203038ec5fe5bbd35209070
sheila <407493486@qq.com>
2018-03-04 23:55:11 +0800

    'phpst'

---
+++ myst/Home/Controller/UserController.class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 40749
+ * Date: 2018/3/4
+ * Time: 23:44
+ */
+namespace Home\Controller;
+
+use Think\Controller;
+
+class UserController extends Controller {
+    public function index() {
+        echo 'dddddd';
+    }
+}
