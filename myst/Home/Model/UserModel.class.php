86231a83abc384a4b1cf3b1357c12ed41858279b
fiveBigniu <407493486@qq.com>
2018-03-07 00:33:37 +0800

    'git记住密码配置'

--- myst/Home/Model/UserModel.class.php
+++ myst/Home/Model/UserModel.class.php
@@ -9,8 +9,8 @@ namespace Home\Model;
 use Think\Model;
 
 class UserModel extends Model{
-    protected $tablePrefix = 'tp_';
-    protected $tableName = 'abc';
-    protected $trueTableName = 'tp_abc';
-    protected $dbName = 'thinkphp';
+//    protected $tablePrefix = 'tp_';
+//    protected $tableName = 'abc';
+//    protected $trueTableName = 'tp_abc';
+//    protected $dbName = 'thinkphp';
 }
