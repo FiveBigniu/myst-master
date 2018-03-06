6b4011d058543a648978be4714d9c6226f0ac509
fiveBigniu <407493486@qq.com>
2018-03-07 00:35:07 +0800

    'setting'

--- myst/Home/Model/UserModel.class.php
+++ myst/Home/Model/UserModel.class.php
@@ -9,6 +9,7 @@ namespace Home\Model;
 use Think\Model;
 
 class UserModel extends Model{
+    //修改表名 前缀 后缀等
 //    protected $tablePrefix = 'tp_';
 //    protected $tableName = 'abc';
 //    protected $trueTableName = 'tp_abc';
